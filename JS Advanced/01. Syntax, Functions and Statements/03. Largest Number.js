f2edeeca78316848dcc2d655c6ee5610ddb5de32
ivohas <105879092+ivohas@users.noreply.github.com>
2022-09-12 21:26:03 +0300

    Create 03. Largest Number.js

---
+++ JS Advanced/01. Syntax, Functions and Statements/03. Largest Number.js
@@ -0,0 +1,14 @@
+function bigestNum(x,y,z){
+if(x>z&&x>y)
+{
+console.log(`The largest number is ${x}.`);
+}
+else if(z>x&&z>y)
+{
+    console.log(`The largest number is ${z}.`);
+}
+else 
+{
+console.log(`The largest number is ${y}.`);
+}
+}
