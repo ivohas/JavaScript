2e5aa760185e9e9163a61a4147dc561e77c6687e
ivohas <105879092+ivohas@users.noreply.github.com>
2022-09-14 11:08:07 +0300

    Create 07. Day of Week.js

---
+++ JS Advanced/01. Syntax, Functions and Statements/07. Day of Week.js
@@ -0,0 +1,27 @@
+function days(input){
+let result='error';
+switch(input){
+case'Monday':
+result=1;
+break;
+case'Tuesday':
+result=2;
+break;
+case'Wednesday':
+result=3;
+break;
+case'Thursday':
+result=4;
+break;
+case'Friday':
+result=5;
+break;
+case'Saturday':
+result=6;
+break;
+case'Sunday':
+result=7;
+break;
+}
+return result;
+}
