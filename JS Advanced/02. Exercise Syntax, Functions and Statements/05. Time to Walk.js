0c7aa83103a240a60b95144846a3456f7437d576
ivohas <105879092+ivohas@users.noreply.github.com>
2022-09-15 19:50:55 +0300

    Update 05. Time to Walk.js

--- JS Advanced/02. Exercise Syntax, Functions and Statements/05. Time to Walk.js
+++ JS Advanced/02. Exercise Syntax, Functions and Statements/05. Time to Walk.js
@@ -1,7 +1,17 @@
 function walk(steps,lengthForStep,speed ){
 let distance= steps*lengthForStep;
-dis
+let bonusTime=  Math.floor(distance/500);
+let speedInMeters= speed/3.6;
+let time= (distance/speedInMeters);
 
-
-console.log(`${hours}:${minutes}:${seconds}`)
+let minute= Math.floor(time/60)
+let seconds= Math.round(time-(minute*60))
+let hours= Math.floor(minute/60)
+function pad(n) {
+    return (n < 10) ? ("0" + n) : n;
 }
+minute+=bonusTime;
+console.log(`${pad(hours)}:${pad(minute)}:${pad(seconds)}`)
+}
+walk(4000, 0.60, 5)
+walk(2564, 0.70, 5.5)
