1675f637e68b13a96fdd5cfb900a3d0ecb72421b
ivohas <105879092+ivohas@users.noreply.github.com>
2022-09-19 18:20:35 +0300

    Update 04. NegativeAndPositive Numbers.js

--- JS Advanced/03. Arrays and Nested Arrays/04. NegativeAndPositive Numbers.js
+++ JS Advanced/03. Arrays and Nested Arrays/04. NegativeAndPositive Numbers.js
@@ -1,16 +1,18 @@
 function solve(arr=[]){
-    Number(arr)
-    arrey=[];
-for(i=0;i<arr.length-1;i++){
-if(arr[i]>=0)
-{
-arrey.push(arr[i])
+    let array = [];
+    for(let number of arr){
+        if(number>=0)
+        {
+            array.push(Number(number))
 
-}else{
-    arrey.unshift(arr[i])
-}
+        }
+        else{
 
-console.log(arrey.join('\n'))
-}
+            array.unshift(Number(number))
+        }
+
+
+    }
+    console.log(array.join('\n'))
 }
 solve([7, -2, 8, 9])
