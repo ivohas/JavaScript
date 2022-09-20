38a88a9c4a9e6ff34290834cf1523ca711feb3d0
ivohas <105879092+ivohas@users.noreply.github.com>
2022-09-20 18:39:44 +0300

    Update 06. Bigger Half.js

--- JS Advanced/03. Arrays and Nested Arrays/06. Bigger Half.js
+++ JS Advanced/03. Arrays and Nested Arrays/06. Bigger Half.js
@@ -1,14 +1,7 @@
-function solve(arr=[]){
+function solve(arr){
 arr.sort(function(a,b){return(a-b)})
-arrey=[]
+
 let a= Math.floor(arr.length/2);
-
-for(i=0;i<arr.length;i++)
-{
-    if(i>=a){
-
-        arrey.push(arr[i])
-    }
-}
-console.log(arrey)
+let result=arr.slice(a)
+return result
 }
