988858af16a07826fe3cfc6089d35f8ae7297dee
ivohas <105879092+ivohas@users.noreply.github.com>
2022-09-22 14:58:14 +0300

    Update 07. Sorting Numbers.js

--- JS Advanced/04. Exercise Arrays and Nested Arrays/07. Sorting Numbers.js
+++ JS Advanced/04. Exercise Arrays and Nested Arrays/07. Sorting Numbers.js
@@ -1,36 +1,24 @@
 function rearrangeArray(arr)
 {
-    // Sorting the array elements
-    arr.sort();
- n= arr.lenght;
-    // To store modified array
-    let tempArr = new Array();
-     
-    // Adding numbers from sorted array 
-    // to new array accordingly
-    let ArrIndex = 0;
- 
-    // Traverse from begin and end simultaneously
-    for (let i = 0, j = n-1; i <= n / 2
-         || j > n / 2; i++, j--) {
-                                     
-        if(ArrIndex < n)
-        {
-            tempArr[ArrIndex] = arr[i];
-            ArrIndex++;
+    result=[]
+    arrey= arr.sort()
+    let i=arr.lenght;
+    while(i>0){
+
+        let a= arrey.pop();
+        result.push(a)
+        i--;
+        if(arrey.lenght<=0){
+            break;
+        
         }
-         
-        if(ArrIndex < n)
-        {
-            tempArr[ArrIndex] = arr[j];
-            ArrIndex++;
-        }
+        i--
+        result.push(arrey.shift())
+
+
+
     }
- 
-    // Modifying original array
-    for (let i = 0; i < n; i++)
-        arr[i] = tempArr[i];
-        return arr
+    return result.join('\n')
 }
  
 console.log(rearrangeArray([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]))
