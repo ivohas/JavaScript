550e9ea5ba9c6f1c6e0dea25eb55764abb41171a
ivohas <105879092+ivohas@users.noreply.github.com>
2022-09-22 14:58:17 +0300

    Create 01. City Record.js

---
+++ JS Advanced/05. Objects and Composition/01. City Record.js
@@ -0,0 +1,12 @@
+function createRecord(name, population, treasury) {
+    return {
+      name,
+      population,
+      treasury
+    };
+  }
+  
+console.log(createRecord('Tortuga',
+7000,
+15000
+))
