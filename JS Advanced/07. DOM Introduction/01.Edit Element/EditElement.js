3bd2dd8237f3fa710a708ba188a6c62bf93704c8
ivohas <105879092+ivohas@users.noreply.github.com>
2022-09-25 15:00:30 +0300

    Update EditElement.js

--- JS Advanced/07. DOM Introduction/01.Edit Element/EditElement.js
+++ JS Advanced/07. DOM Introduction/01.Edit Element/EditElement.js
@@ -1,3 +1,9 @@
-function editElement() {
+function editElement(ref, match,replacer) {
     // TODO
+    
+    const content = ref.textContent;
+    const matcher = new RegExp(match, 'g');
+    const edited = content.replace(matcher, replacer);
+    ref.textContent = edited;
+  
 }
