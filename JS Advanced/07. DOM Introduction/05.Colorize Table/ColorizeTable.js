b0cc72bdec9b3f92e594a11ef7fba8136cf85e7c
ivohas <105879092+ivohas@users.noreply.github.com>
2022-10-03 18:48:56 +0300

    Update ColorizeTable.js

--- JS Advanced/07. DOM Introduction/05.Colorize Table/ColorizeTable.js
+++ JS Advanced/07. DOM Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,11 @@
 function colorize() {
     // TODO
+    let arrey = document.querySelectorAll('table tr')
+     index=0
+    for (const iterator of arrey) {
+      index++ 
+        if(index%2==0){
+           iterator.style.background='teal'
+        }
+    }
 }
