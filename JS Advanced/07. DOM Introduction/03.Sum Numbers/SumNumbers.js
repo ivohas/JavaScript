d524d8250916b20ac37b18f6df30fffcdc4b950c
ivohas <105879092+ivohas@users.noreply.github.com>
2022-10-03 18:48:49 +0300

    Update SumNumbers.js

--- JS Advanced/07. DOM Introduction/03.Sum Numbers/SumNumbers.js
+++ JS Advanced/07. DOM Introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,7 @@
 function calc() {
     // TODO: sum = num1 + num2
+    let firstNum= document.getElementById('num1').value
+    let secondNum= document.getElementById('num2').value
+    let result = Number(firstNum)+Number(secondNum)
+    document.getElementById('sum').value=result
 }
