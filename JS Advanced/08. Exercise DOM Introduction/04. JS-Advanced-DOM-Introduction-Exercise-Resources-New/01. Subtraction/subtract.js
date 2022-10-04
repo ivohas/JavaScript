d374cdf9ebe9f157e33bba5437a7b5f971979b85
ivohas <105879092+ivohas@users.noreply.github.com>
2022-10-04 18:32:56 +0300

    Create subtract.js

---
+++ JS Advanced/08. Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -0,0 +1,8 @@
+function subtract() {
+   // console.log('TODO:...');
+   let firstNum= Number(document.getElementById('firstNumber').value)
+   let secondElement=Number(document.getElementById('secondNumber').value)
+   let result = firstNum-secondElement;
+   document.getElementById('result').innerText=result
+
+}
