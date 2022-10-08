aafa9527118b3fa1f80f0b820b9292f3bbfc8591
ivohas <105879092+ivohas@users.noreply.github.com>
2022-10-08 11:45:17 +0300

    Update search.js

--- JS Advanced/08. Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ JS Advanced/08. Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
    // TODO
+   //`${matches} matches found`.
+   let townToSearch= document.getElementById("searchText").value
+   let towns =Array.from(document.querySelectorAll('ul li'))
+   debugger
+   let matches=0
+   for (const town of towns) {
+      text = town.textContent
+      if(text.includes(townToSearch))
+      {
+         town.style.textDecoration='underline'
+         town.style.fontWeight="bold"
+         matches++;
+      }
+   }
+   if(matches>0){
+      document.getElementById('result').textContent=`${matches} matches found`
+   }
+   
 }
