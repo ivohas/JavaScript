6fed67376fb7f66153222d060fc4131fafa9ba35
ivohas <105879092+ivohas@users.noreply.github.com>
2022-10-08 11:45:10 +0300

    Update accordion.js

--- JS Advanced/08. Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ JS Advanced/08. Exercise DOM Introduction/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,12 +1,14 @@
 function toggle() {
     //console.log('TODO:...');
-   
-    let button = document.getElementsByClassName('button')[0];
-  
-   if(button.textContent=="More"){
-      button.innerText=="Less"
-   }else{
-      button.innerText=='More'
-   }
-   
+    let button = document.querySelector('span.button');
+    let divHiden = document.getElementById('extra');
+    if (button.textContent == 'More') {
+        divHiden.style.display = 'block';
+        button.textContent = 'Less';
+    } else if (button.textContent == 'Less') {
+        divHiden.style.display = 'none';
+        button.textContent = 'More';
+    }
 }
+    
+   
