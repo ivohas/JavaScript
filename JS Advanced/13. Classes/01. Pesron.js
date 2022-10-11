23d1613322b6ef02783c3aecd0cec160c480d855
ivohas <105879092+ivohas@users.noreply.github.com>
2022-10-11 18:48:09 +0300

    Create 01. Pesron.js

---
+++ JS Advanced/13. Classes/01. Pesron.js
@@ -0,0 +1,11 @@
+class PersonInfo{
+    constructor(firstName,lastName,age,email){
+        this.firstName=firstName
+        this.lastName=lastName
+        this.age=age
+        this.email=email
+    }
+    toString=()=>{
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+    }
+}
