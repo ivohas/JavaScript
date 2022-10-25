fdd8ed0e0e609aa9a0b15bb84b52e3194dfad4b8
ivohas <105879092+ivohas@users.noreply.github.com>
2022-10-25 17:07:44 +0300

    Update 02. Smart Hike.js

--- JS Advanced/17. Exam Preparation/02. Smart Hike.js
+++ JS Advanced/17. Exam Preparation/02. Smart Hike.js
@@ -22,20 +22,76 @@ class SmartHike{
         }
         let difference= this.resources-time*10
         if(difference<0){
-            throw Error("You don't have enough resources to complete the hike")
+           return "You don't have enough resources to complete the hike"
         }
         this.resources-=time*10
         this.listOfHikes.push({ peak, time, difficultyLevel })
         return `You hiked ${peak} peak for ${time} hours and you have ${this.resources}% resources left`
     }
-} 
-//test 2
+    rest =(time)=>{
+        this.resources=this.resources+time*10
+        if(this.resources>=100){
+            return `Your resources are fully recharged. Time for hiking!`
+        }
+        return `You have rested for ${time} hours and gained ${time*10}% resources`
+    }
+    showRecord= (criteria)=>{
+        if (this.listOfHikes.length == 0) {
+            return `${this.username} has not done any hiking yet`;
+          }
+      
+          if (criteria === "hard" || criteria === "easy") {
+            let allHikes = this.listOfHikes.filter(
+              (hike) => hike.difficultyLevel === criteria
+            );
+            let sortedHikes = allHikes.sort((a, b) => a.time - b.time);
+            let bestHike = sortedHikes[0];
+      
+            if (bestHike === undefined) {
+              return `${this.username} has not done any ${criteria} hiking yet`;
+            }
+      
+            return `${this.username}'s best ${criteria} hike is ${bestHike.peak} peak, for ${bestHike.time} hours`;
+          } else if (criteria === "all") {
+            let result = ["All hiking records:"];
+            this.listOfHikes.forEach((hike) => {
+              result.push(
+                `${this.username} hiked ${hike.peak} for ${hike.time} hours`
+              );
+            });
+      
+            return result.join("\n");
+          }
+        }
+      }
+
+
+// test 4
 const user = new SmartHike('Vili');
-console.log(user.addGoal('Musala', 2925));
-console.log(user.addGoal('Rui', 1706));
-console.log(user.hike('Musala', 8, 'hard'));
-console.log(user.hike('Rui', 3, 'easy'));
-console.log(user.hike('Everest', 12, 'hard'));
+user.addGoal('Musala', 2925);
+user.hike('Musala', 8, 'hard');
+console.log(user.showRecord('easy'));
+user.addGoal('Vihren', 2914);
+user.hike('Vihren', 4, 'hard');
+console.log(user.showRecord('hard'));
+user.addGoal('Rui', 1706);
+user.hike('Rui', 3, 'easy');
+console.log(user.showRecord('all'));
+
+//test 3
+// const user = new SmartHike('Vili');
+// console.log(user.addGoal('Musala', 2925));
+// console.log(user.hike('Musala', 8, 'hard'));
+// console.log(user.rest(4));
+// console.log(user.rest(5));
+
+//test 2
+// const user = new SmartHike('Vili');
+// console.log(user.addGoal('Musala', 2925));
+// console.log(user.addGoal('Rui', 1706));
+// console.log(user.hike('Musala', 8, 'hard'));
+// console.log(user.hike('Rui', 3, 'easy'));
+// console.log(user.hike('Everest', 12, 'hard'));
 
 // test 1
 // const user = new SmartHike('Vili');
