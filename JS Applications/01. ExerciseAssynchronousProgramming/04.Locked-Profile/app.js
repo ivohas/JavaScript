5dbd42d68edf9eee58e4ab20587848a0bdd4f7f2
ivohas <105879092+ivohas@users.noreply.github.com>
2022-11-25 15:53:16 +0200

    Create app.js

---
+++ JS Applications/01. ExerciseAssynchronousProgramming/04.Locked-Profile/app.js
@@ -0,0 +1,6 @@
+function lockedProfile() {
+    console.log('TODO...')
+    let posts= "http://localhost:3030/jsonstore/blog/posts";
+    let comments="http://localhost:3030/jsonstore/blog/posts";
+
+}
