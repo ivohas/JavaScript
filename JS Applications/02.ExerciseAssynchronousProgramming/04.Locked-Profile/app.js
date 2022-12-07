b73f343c8e21e8bd9929cfffa0cb8300ade17147
ivohas <105879092+ivohas@users.noreply.github.com>
2022-12-07 21:13:26 +0200

    Delete app.js

--- JS Applications/02.ExerciseAssynchronousProgramming/04.Locked-Profile/app.js
+++
@@ -1,6 +0,0 @@
-function lockedProfile() {
-    console.log('TODO...')
-    let posts= "http://localhost:3030/jsonstore/blog/posts";
-    let comments="http://localhost:3030/jsonstore/blog/posts";
-
-}
