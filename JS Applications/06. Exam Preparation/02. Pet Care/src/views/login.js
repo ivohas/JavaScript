2dc569be0d17e8361d93d9ce8a74d5d61cef622e
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-19 15:00:32 +0300

    Create login.js

---
+++ JS Applications/06. Exam Preparation/02. Pet Care/src/views/login.js
@@ -0,0 +1,43 @@
+import { html } from ' ../node_modules/lit-html/lit-html.js'
+import {login} from '../api/user.js'
+import { createSubmitHandler } from '../util.js'
+
+
+const loginTemplate = (onLogin) => html`
+<section id="loginPage">
+    <form @submit=${onLogin} class="loginForm">
+        <img src="./images/logo.png" alt="logo" />
+        <h2>Login</h2>
+
+        <div>
+            <label for="email">Email:</label>
+            <input id="email" name="email" type="text" placeholder="steven@abv.bg" value="">
+        </div>
+
+        <div>
+            <label for="password">Password:</label>
+            <input id="password" name="password" type="password" placeholder="********" value="">
+        </div>
+
+        <button class="btn" type="submit">Login</button>
+
+        <p class="field">
+            <span>If you don't have profile click <a href="/register">here</a></span>
+        </p>
+    </form>
+</section>`
+
+export function showLogin(ctx){
+  ctx.render(loginTemplate(createSubmitHandler(onLogin)))
+
+   async function onLogin({email,password}){
+ 
+    if(email==''|| password==''){
+      return alert("All fields are required!")
+
+    }
+    await login(email,password);
+    ctx.updateNav()
+    ctx.page.redirect('/')
+   }
+}
