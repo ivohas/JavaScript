68bf6008f1eff50c8e4d2498de949d984bf0acb5
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-21 09:54:50 +0300

    Create user.js

---
+++ JS Applications/06. Exam Preparation/04. Music App/src/api/user.js
@@ -0,0 +1,34 @@
+import { clearUserData, setUserData } from "../util.js";
+import { get, post } from "./api.js";
+
+const endpoint={
+    'login': '/users/login',
+    'register': '/users/register',
+    'logout':'/users/logout'
+    // may have more endpoins or have one different
+}
+//  there can be difference in pasword or email 
+export async function login(email, password){
+    const { _id, email: resultEmail, accessToken} = await post( '/users/login', { email, password });
+
+    setUserData({
+        _id,
+        email: resultEmail,
+        accessToken
+    });
+}
+
+export async function register(email, password){
+    const { _id, email: resultEmail, accessToken} = await post(endpoint.register, { email, password });
+
+    setUserData({
+        _id,
+        email: resultEmail,
+        accessToken
+    });
+}
+
+export async function logout(){
+    get(endpoint.logout);
+    clearUserData();
+}
