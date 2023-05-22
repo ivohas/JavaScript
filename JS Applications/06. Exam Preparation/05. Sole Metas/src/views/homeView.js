459044306a78ab3313296c487cef9d6f37738a2d
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-22 16:04:03 +0300

    Create homeView.js

---
+++ JS Applications/06. Exam Preparation/05. Sole Metas/src/views/homeView.js
@@ -0,0 +1,15 @@
+import { html } from '../lib.js'
+
+const homeTemp = () => html`
+<section id="home">
+    <h1>Welcome to Sole Mates</h1>
+    <img src="./images/home.jpg" alt="home" />
+    <h2>Browse through the shoe collectibles of our users</h2>
+    <h3>Add or manage your items</h3>
+</section>
+`
+
+export async function showHome(ctx) {
+
+    ctx.render(homeTemp())
+}
