e4d7889b0887b28e72ea94a49a0d557b446770ac
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-22 16:03:46 +0300

    Create app.js

---
+++ JS Applications/06. Exam Preparation/05. Sole Metas/src/app.js
@@ -0,0 +1,53 @@
+
+import {render, page} from './lib.js'
+import {getUserData} from './util.js'
+import { showCatalog } from './views/catalogView.js'
+import { showCreate } from './views/createView.js'
+import { showEdit } from './views/editView.js'
+import { showHome } from './views/homeView.js'
+import { showDetails } from './views/itemDetails.js'
+import { showLogin } from './views/loginView.js'
+import {updateNav} from './views/nav.js'
+import { showRegister } from './views/registerView.js'
+// Import inn the main html src app.js
+// Make second 
+
+
+// Import views
+
+// get main element 
+const main= document.querySelector('main')
+
+//  Attach middle ware. May hav some difference
+page(decorateContext);
+page('/',showHome)
+page('/home',showHome)
+page('/catalog', showCatalog)
+page('/catalog/:id',showDetails );
+page('/edit/:id', showEdit);
+page('/create', showCreate);
+page('/login', showLogin);
+page('/register', showRegister);
+
+
+// update Navigation and start page
+updateNav()
+page.start()
+
+
+
+function decorateContext(ctx, next){
+    ctx.render = renderMain;
+    ctx.updateNav = updateNav;
+
+    const user = getUserData();
+    if(user){
+        ctx.user = user;
+    }
+    
+    next();
+}
+
+function renderMain(content){
+    render(content, main);
+}
