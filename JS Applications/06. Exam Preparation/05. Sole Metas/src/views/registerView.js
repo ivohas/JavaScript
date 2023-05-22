5893fa977ea3ec612fc2c7871d71db1ba55cd9ee
ivohas <105879092+ivohas@users.noreply.github.com>
2023-05-22 16:04:12 +0300

    Create registerView.js

---
+++ JS Applications/06. Exam Preparation/05. Sole Metas/src/views/registerView.js
@@ -0,0 +1,37 @@
+import { html } from '../lib.js'
+import {createSubmitHandler} from '../util.js'
+import {register} from '../api/user.js'
+
+const registerTemp = (heandler) => html`
+<section id="register">
+    <div class="form">
+        <h2>Register</h2>
+        <form @submit=${heandler} class="login-form">
+            <input type="text" name="email" id="register-email" placeholder="email" />
+            <input type="password" name="password" id="register-password" placeholder="password" />
+            <input type="password" name="re-password" id="repeat-password" placeholder="repeat password" />
+            <button type="submit">login</button>
+            <p class="message">Already registered? <a href="/login">Login</a></p>
+        </form>
+    </div>
+</section>
+`
+
+export async function showRegister(ctx) {
+    ctx.render(registerTemp(createSubmitHandler(onRegister)))
+
+    async function onRegister(data) {
+
+        if (data.email == "" || data.password == "" || data["re-password"]== "") {
+            return alert("all fields is require")
+
+        } else {
+            if (data.password !== data["re-password"]) {
+                return alert("password don\'t match")
+            }
+        }
+       await register(data.email, data.password)
+        ctx.updateNav()
+        ctx.page.redirect('/catalog')
+    }
+}
